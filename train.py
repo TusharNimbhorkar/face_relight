efa9d7e831a75c676b46d2b9ec0905ae83fec796
NSavov <nedko066@gmail.com>
2019-12-04 14:36:23 +0100

    Timing changes

--- train.py
+++ train.py
@@ -1,4 +1,7 @@
 import time,sys
+
+import torch
+
 from options.train_options import TrainOptions
 from data import CreateDataLoader
 from models import create_model
@@ -13,8 +16,19 @@ if __name__ == '__main__':
     total_steps = 0
 
     for epoch in range(1,15):
-        epoch_start_time = time.time()
-        iter_data_time = time.time()
+        epoch_start_time = torch.cuda.Event(enable_timing=True)
+        epoch_end_time = torch.cuda.Event(enable_timing=True)
+
+        iter_start_time = torch.cuda.Event(enable_timing=True)
+        iter_end_time = torch.cuda.Event(enable_timing=True)
+
+        data_start_time = torch.cuda.Event(enable_timing=True)
+        data_end_time = torch.cuda.Event(enable_timing=True)
+
+        epoch_start_time.record()
+        data_start_time.record()
+
+
         epoch_iter = 0
         data_loader = CreateDataLoader(opt)
         dataset = data_loader.load_data()
@@ -22,11 +36,12 @@ if __name__ == '__main__':
         print('#training images = %d' % dataset_size)
 
         for i, data in enumerate(dataset):
-            iter_start_time = time.time()
-            if total_steps % opt.print_freq == 0:
-                t_data = iter_start_time - iter_data_time
-            visualizer.reset()
             total_steps += opt.batch_size
+            if total_steps % opt.print_freq == 0:
+                iter_start_time.record()
+                data_end_time.record()
+            visualizer.reset()
+
             epoch_iter += opt.batch_size
             # print('set_input')
             model.set_input(data)
@@ -40,8 +55,12 @@ if __name__ == '__main__':
 
             if total_steps % opt.print_freq == 0:
                 losses = model.get_current_losses()
-                t = (time.time() - iter_start_time) / opt.batch_size
-                visualizer.print_current_losses(epoch, epoch_iter, losses, t, t_data)
+                iter_end_time.record()
+                torch.cuda.synchronize(model.device)
+                t = (iter_start_time.elapsed_time(iter_end_time)/1000) / opt.batch_size
+                t_data = (data_start_time.elapsed_time(data_end_time)/1000) / opt.batch_size
+                t_current = ((epoch_start_time.elapsed_time(iter_end_time) / 1000) / opt.batch_size)/epoch_iter
+                visualizer.print_current_losses(epoch, epoch_iter, losses, t, t_data, {'Avg Time: %.3f':t_current})
                 if opt.display_id > 0:
                     visualizer.plot_current_losses(epoch, float(epoch_iter) / dataset_size, opt, losses)
 
@@ -50,12 +69,17 @@ if __name__ == '__main__':
                 save_suffix = 'iter_%d' % total_steps if opt.save_by_iter else 'latest'
                 model.save_networks(save_suffix)
 
-            iter_data_time = time.time()
+
+            if (total_steps+opt.batch_size) % opt.print_freq == 0:
+                data_start_time.record()
+
         if epoch % opt.save_epoch_freq == 0:
             print('saving the model at the end of epoch %d, iters %d' % (epoch, total_steps))
             model.save_networks('latest')
             model.save_networks(epoch)
 
+        epoch_end_time.record()
+        torch.cuda.synchronize(model.device)
         print('End of epoch %d / %d \t Time Taken: %d sec' %
-              (epoch, opt.niter + opt.niter_decay, time.time() - epoch_start_time))
+              (epoch, opt.niter + opt.niter_decay,  epoch_start_time.elapsed_time(epoch_end_time)/1000))
         model.update_learning_rate()
