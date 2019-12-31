80543d2ab5e869b62a2817ba8ffcf17134518349
tushar1408 <tushar.1408@gmail.com>
2019-12-31 12:59:12 +0100

    check with only cuda

--- models/base_model.py
+++ models/base_model.py
@@ -14,7 +14,9 @@ class BaseModel(ABC):
         self.opt = opt
         self.gpu_ids = opt.gpu_ids
         self.isTrain = opt.isTrain
-        self.device = torch.device('cuda:{}'.format(self.gpu_ids[0])) if self.gpu_ids else torch.device('cpu')
+        # self.device = torch.device('cuda:{}'.format(self.gpu_ids[0])) if self.gpu_ids else torch.device('cpu')
+        self.device = torch.device('cuda') if self.gpu_ids else torch.device('cpu')
+
         self.save_dir = os.path.join(opt.checkpoints_dir, opt.name)
         if opt.resize_or_crop != 'scale_width':
             torch.backends.cudnn.benchmark = True
