0365961173bac75c68bee15a817862127b83c9b9
tushar1408 <tushar.1408@gmail.com>
2020-01-17 13:55:38 +0100

    reformat code and TODO:paths to dataset use properly

--- demo_eval.py
+++ demo_eval.py
@@ -16,7 +16,6 @@ import torch
 import cv2
 import joblib
 
-
 ap = argparse.ArgumentParser()
 ap.add_argument('--frontal', dest='frontal', action='store_true')
 ap.set_defaults(feature=True)
@@ -27,44 +26,38 @@ checkpoint_dir_cmd = 'models/trained/14_net_G_dpr7_mseBS20.pth'
 '''
 functions to calculate mean squared error
 '''
-def mse(imageA, imageB):
 
+
+def mse(imageA, imageB):
     err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
     err /= float(imageA.shape[0] * imageA.shape[1])
     rmse = math.sqrt(err)
     return rmse
 
 
-def mse_all(i1,i2):
-    mse_r = mse(i1[:, :, 0], i2[:, :, 0])
-    mse_g = mse(i1[:, :, 1], i2[:, :, 1])
-    mse_b = mse(i1[:, :, 2], i2[:, :, 2])
-
-    return (mse_r+mse_g+mse_b)/3.0
-
-def mse_seg(imageA, imageB,seg):
-
+def mse_seg(imageA, imageB, seg):
     err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
-    err /= float(np.sum(seg[:,:,0]))
+    err /= float(np.sum(seg[:, :, 0]))
     rmse = math.sqrt(err)
     return rmse
 
 
-def mse_all_seg(i1,i2,seg):
-    mse_r = mse_seg(i1[:, :, 0], i2[:, :, 0],seg)
-    mse_g = mse_seg(i1[:, :, 1], i2[:, :, 1],seg)
-    mse_b = mse_seg(i1[:, :, 2], i2[:, :, 2],seg)
+def mse_all_seg(i1, i2, seg):
+    mse_r = mse_seg(i1[:, :, 0], i2[:, :, 0], seg)
+    mse_g = mse_seg(i1[:, :, 1], i2[:, :, 1], seg)
+    mse_b = mse_seg(i1[:, :, 2], i2[:, :, 2], seg)
 
-    return (mse_r+mse_g+mse_b)/3.0
+    return (mse_r + mse_g + mse_b) / 3.0
+
 
 def id_to_degrees(id):
     return (int(id) - 7) * 15
+
 
 '''
 end
 
 '''
-
 
 modelFolder = 'trained_model/'
 
@@ -84,21 +77,19 @@ persons = os.listdir(test_dir)
 sh_poly_path_temp = 'models/poly_%s_7.joblib'
 sh_linear_path_temp = 'models/linear_%s_7.joblib'
 
-
 # load the two input images
-from_id_list = [8,9,10,6,5,4]
-IMS = ['_08.png','_09.png','_10.png','_06.png','_05.png','_04.png',]
+from_id_list = [8, 9, 10, 6, 5, 4]
+IMS = ['_08.png', '_09.png', '_10.png', '_06.png', '_05.png', '_04.png', ]
 device = "cuda"
 
 for ii in range(len(from_id_list)):
 
-    if args['frontal']:            
+    if args['frontal']:
         from_id = 7
         to_id = from_id_list[ii]
     else:
         from_id = from_id_list[ii]
         to_id = 7
-
 
     if from_id == 7:
         pose = id_to_degrees(to_id)
@@ -111,7 +102,6 @@ for ii in range(len(from_id_list)):
     else:
         raise ValueError()
 
-
     poly = joblib.load(sh_poly_path)
     lin = joblib.load(sh_linear_path)
     feat = poly.transform([[pose]])
@@ -123,10 +113,10 @@ for ii in range(len(from_id_list)):
     overall_error = 0.0
 
     for per in persons:
-        
+
         person_dir = os.path.join(test_dir, per)
         # from
-        if args['frontal']:            
+        if args['frontal']:
             side_im = os.path.join(person_dir, per + '_07.png')
             front_im = os.path.join(person_dir, per + front_number)
             from_sh = 0
@@ -134,17 +124,15 @@ for ii in range(len(from_id_list)):
             # print('pose',pose)
 
         else:
-            side_im = os.path.join(person_dir, per + front_number)        
+            side_im = os.path.join(person_dir, per + front_number)
             front_im = os.path.join(person_dir, per + '_07.png')
             from_sh = pose
             to_sh = 0
 
-
         exists_ims_side = cv2.imread(side_im) is not None
         exists_ims_front = cv2.imread(front_im) is not None
 
-        if os.path.exists(side_im) and os.path.exists(front_im) and exists_ims_front and exists_ims_side:            
-
+        if os.path.exists(side_im) and os.path.exists(front_im) and exists_ims_front and exists_ims_side:
 
             dir_ims = 'test_data/relight_constant'
             ims = os.listdir(dir_ims)
@@ -183,7 +171,7 @@ for ii in range(len(from_id_list)):
                 sh = Variable(torch.from_numpy(sh).cuda())
                 # --------------------------------------------------
                 _, _, outputSH, _ = my_network(inputL1, sh, 0)
-                outputImg, _, _, _ = my_network(inputL, outputSH*sh_constant_, 0)
+                outputImg, _, _, _ = my_network(inputL, outputSH * sh_constant_, 0)
                 # --------------------------------------------------
 
                 outputImg = outputImg[0].cpu().data.numpy()
@@ -198,7 +186,7 @@ for ii in range(len(from_id_list)):
                 segment_path_ear = os.path.join('/home/tushar/data2/face-parsing.PyTorch/res/', 'mpie_segment',
                                                 per,
                                                 per + front_number)
-                im1 = cv2.imread(segment_path_ear,0)
+                im1 = cv2.imread(segment_path_ear, 0)
                 im1[im1 == 8] = 255
                 im1[im1 == 7] = 255
                 im1[im1 < 255] = 0
@@ -234,18 +222,14 @@ for ii in range(len(from_id_list)):
                 segment_im[segment_im > 0] = 1
                 resize = True
                 if resize:
-                    img_side_copy = cv2.resize(img_side_copy,(128,128))
-                    segment_im = cv2.resize(segment_im,(128,128))
-                    resultLab = cv2.resize(resultLab,(128,128))
+                    img_side_copy = cv2.resize(img_side_copy, (128, 128))
+                    segment_im = cv2.resize(segment_im, (128, 128))
+                    resultLab = cv2.resize(resultLab, (128, 128))
 
-                current_mse = mse_all_seg(np.multiply(img_side_copy,segment_im), np.multiply(resultLab,segment_im),segment_im)
+                current_mse = mse_all_seg(np.multiply(img_side_copy, segment_im), np.multiply(resultLab, segment_im),
+                                          segment_im)
                 overall_error = overall_error + current_mse
-                number_files=number_files+1
+                number_files = number_files + 1
 
-
-    print('\nRMSE Score for transfering light of ',from_sh,' to ', to_sh )
-    print("rmse(al_segment): ", (overall_error/number_files))
-
-
-
-
+    print('\nRMSE Score for transfering light of ', from_sh, ' to ', to_sh)
+    print("rmse(al_segment): ", (overall_error / number_files))
