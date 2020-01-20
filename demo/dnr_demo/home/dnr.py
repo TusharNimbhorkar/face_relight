bcf31672b6db9435b708f270189963671eec7ca9
NSavov <nedko066@gmail.com>
2020-01-20 20:48:53 +0100

    Multiple images handling

--- demo/dnr_demo/home/dnr.py
+++ demo/dnr_demo/home/dnr.py
@@ -63,56 +63,56 @@ def init_gpu(data_path, model_path):
         sh_lookup = sh_lookups['horizontal']
 
 @shared_task
-def prediction_task(data_path, img_path, x,y):
+def prediction_task(data_path, img_path):
     global sh_lookup, base_model
     worker_device = get_device()
 
-    sh = sh_lookup[x]
+    dir_uuid = str(uuid.uuid1())
+    out_dir = osp.join(data_path, 'output', dir_uuid)
+    os.makedirs(out_dir, exist_ok=True)
 
-    filename_uuid = str(uuid.uuid1())
-    input_img_path = img_path
-    # --------------------------------------------------
-    # rendering half-sphere
-    sh_mul = 0.8
-    sh = np.squeeze(sh) * sh_mul
-    sh = np.reshape(sh, (1, 9, 1, 1)).astype(np.float32)
-    sh = Variable(torch.from_numpy(sh).to(worker_device))
+    for preset_id, sh_presets in sh_lookups.items():
+        for i, sh in enumerate(sh_presets):
+            sh_mul = 0.8
+            sh = np.squeeze(sh) * sh_mul
+            sh = np.reshape(sh, (1, 9, 1, 1)).astype(np.float32)
+            sh = torch.autograd.Variable(torch.from_numpy(sh).to(worker_device))
 
-    img = cv2.imread(input_img_path)
-    row, col, _ = img.shape
-    img = cv2.resize(img, (512, 512))
-    Lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+            img = cv2.imread(img_path)
+            row, col, _ = img.shape
+            img = cv2.resize(img, (512, 512))
+            Lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
 
-    inputL = Lab[:, :, 0]
-    inputL = inputL.astype(np.float32) / 255.0
-    inputL = inputL.transpose((0, 1))
-    inputL = inputL[None, None, ...]
-    inputL = Variable(torch.from_numpy(inputL).to(worker_device))
+            inputL = Lab[:, :, 0]
+            inputL = inputL.astype(np.float32) / 255.0
+            inputL = inputL.transpose((0, 1))
+            inputL = inputL[None, None, ...]
+            inputL = torch.autograd.Variable(torch.from_numpy(inputL).to(worker_device))
 
-    outputImg, _, outputSH, _ = base_model(inputL, sh, 0)
+            outputImg, _, outputSH, _ = base_model(inputL, sh, 0)
 
-    outputImg = outputImg[0].cpu().data.numpy()
-    outputImg = outputImg.transpose((1, 2, 0))
-    outputImg = np.squeeze(outputImg)  # *1.45
-    outputImg = (outputImg * 255.0).astype(np.uint8)
-    Lab[:, :, 0] = outputImg
-    resultLab = cv2.cvtColor(Lab, cv2.COLOR_LAB2BGR)
-    resultLab = cv2.resize(resultLab, (col, row))
+            outputImg = outputImg[0].cpu().data.numpy()
+            outputImg = outputImg.transpose((1, 2, 0))
+            outputImg = np.squeeze(outputImg)  # *1.45
+            outputImg = (outputImg * 255.0).astype(np.uint8)
+            Lab[:, :, 0] = outputImg
+            resultLab = cv2.cvtColor(Lab, cv2.COLOR_LAB2BGR)
+            resultLab = cv2.resize(resultLab, (col, row))
 
-    filename = filename_uuid +".png"
-    cv2.imwrite(osp.join(data_path,'output/'+filename), resultLab)
+            filename = preset_id + '_' + str(i) + '.png'
+            cv2.imwrite(osp.join(out_dir, filename), resultLab)
 
-    return filename
+    return dir_uuid
 
 
 # replace this function with your own
 # returns the classification result of a given image_path
-def process_image(img_path, x_id, y_id=0):
+def process_image(img_path):
 
     # global data_path, src_data_path, model_path
     #
     data_path = osp.abspath('../data/')
-    task = prediction_task.delay(data_path, img_path, x_id, y_id)
+    task = prediction_task.delay(data_path, img_path)
 
     return task.get()
 
@@ -125,3 +125,4 @@ def worker_process_init_(**kwargs):
     data_path = osp.abspath('../data/')
     model_path = osp.join(data_path, "model/14_net_G_dpr7_mseBS20.pth")
     init_gpu(data_path, model_path)  # make sure all models are initialized upon starting the worker
+    # prediction_task(data_path, '../../test_data/portrait_/a1.jpeg', 0,0)
