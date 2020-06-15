501c361a45e0c05ff48fae87007c5a8127d90a61
nsavov <nedko066@gmail.com>
2020-06-15 20:47:11 +0200

    Template for processing pipeline

---
+++ process_dataset_template.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+bash ./process_dataset_exec.sh \
+ /home/nedko/face_relight/dbs/data/stylegan_v0/test 1024 \
+ /home/nedko/face_relight/dbs/stylegan_test 256 \
+ /home/tushar/data2/rendering_pipeline/stylegan_final_30k \
+ /home/nedko/face_relight/dbs/data/real/real_ffhq_256 \
+ use_sh_gen
