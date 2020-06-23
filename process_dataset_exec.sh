efab2adf751149471a1236b1e3197aa6f7099675
nsavov <nedko066@gmail.com>
2020-06-24 01:18:47 +0200

    Dataset processing error handling and option for overwriting existing folders

--- process_dataset_exec.sh
+++ process_dataset_exec.sh
@@ -7,35 +7,60 @@ TGT_SZ=$4
 ORIG_PATH=$5
 REAL_PATH=$6
 
-USE_SH_GEN=$7
+OPTIONS=$7
+
+USE_GEN_SH=false
+USE_OVERWRITE=false
+
+if [[ $OPTIONS == *"use_gen_sh"* ]]; then
+    USE_GEN_SH=true
+fi
+
+if [[ $OPTIONS == *"use_overwrite"* ]]; then
+    USE_OVERWRITE=true
+fi
+
 
 ROOT_PATH=`dirname $SRC_PATH`
 SRC_BASENAME=`basename $SRC_PATH`
 REAL_BASENAME=`basename $REAL_PATH`
 
 CROP_PATH=$ROOT_PATH/${SRC_BASENAME}_crop
-RESIZE_PATH=$ROOT_PATH/${SRC_BASENAME}_crop_${TGT_SZ}
+RESIZE_PATH=${CROP_PATH}_${TGT_SZ}
 FACE_DATA_PATH=$ROOT_PATH/face_data
 
-if [ "$USE_SH_GEN" = "use_sh_gen" ]; then
+try()
+{
+    LAST_EXIT_CODE=$?
+    echo $LAST_EXIT_CODE
+    if [[ $LAST_EXIT_CODE -ne 0 ]]; then
+        echo "An Error Has Occurred!"
+        exit $LAST_EXIT_CODE
+    fi
+}
+
+if $USE_GEN_SH ; then
     printf "\nGenerating SH and copying original images"
     python gen_sh.py -i $SRC_PATH -p $ORIG_PATH -s $SRC_SZ
+    try
 else
     printf "\nSkipping sh generation and original image copy."
 fi
 
-if [[ -e $CROP_PATH ]]; then
+if [[ $USE_OVERWRITE == false && -e $CROP_PATH && "$(ls -A ${CROP_PATH})" ]]; then
     printf "\nCrop folder exists."
 else
     printf "\nCropping the dataset..."
     python crop_dataset.py -i $SRC_PATH -o $CROP_PATH -f $FACE_DATA_PATH
+    try
 fi
 
-if [[ -e $RESIZE_PATH ]]; then
+if [[ $USE_OVERWRITE == false && -e $RESIZE_PATH && "$(ls -A ${RESIZE_PATH})" ]]; then
     printf "\nResize folder exists."
 else
     printf "\nResizing the dataset..."
     python resize_dataset.py -i $CROP_PATH -o $RESIZE_PATH -s $TGT_SZ --no_real --no_segments
+    try
 fi
 
 
--- process_dataset_template.sh
+++ process_dataset_template.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
+#The last parameter is a string of options divided by spaces:
+#use_gen_sh - sh files will be generated in the source directory prior to proceeding
+#use_overwrite - do not perform checks if cropped or resized folders exist - this will overwrite existing files
+
 bash ./process_dataset_exec.sh \
  /home/nedko/face_relight/dbs/data/stylegan_v0/test 1024 \
  /home/nedko/face_relight/dbs/stylegan_test 256 \
  /home/tushar/data2/rendering_pipeline/stylegan_final_30k \
  /home/nedko/face_relight/dbs/data/real/real_ffhq_256 \
- use_sh_gen
+ "use_gen_sh"
