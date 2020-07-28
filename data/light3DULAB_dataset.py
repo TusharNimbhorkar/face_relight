2095623104bfbd9d304f22a3950d83e2c4257492
tushar1408 <tushar.1408@gmail.com>
2020-07-29 00:46:21 +0200

    add parameter to read second real_im for  D2

--- data/light3DULAB_dataset.py
+++ data/light3DULAB_dataset.py
@@ -9,6 +9,8 @@ class light3DULABDataset(lightDPR7Dataset):
     def __init__(self, opt):
         super(light3DULABDataset,self).__init__(opt)
         self.use_large_real_set = os.path.exists(os.path.join(self.opt.dataroot, 'real_im', "{:06d}".format(0) + '.png'))
+        if self.opt.load_realD2 is not None:
+            self.use_large_real_set2 = os.path.exists(os.path.join(self.opt.load_realD2, "{:06d}".format(0) + '.png'))
 
     def _get_item_path(self, path, fname_pattern):
         dir, fname = path.rsplit('/',1)
@@ -16,7 +18,7 @@ class light3DULABDataset(lightDPR7Dataset):
         path = os.path.join(dir, fname_pattern % id)
         return path
 
-    def _get_paths(self, real_img_id, input_img_id):
+    def _get_paths(self, real_img_id, input_img_id, real_img_id2):
         AB_path = self.list_AB[input_img_id]
         source_path = AB_path[0]
         target_path = AB_path[1]
@@ -29,10 +31,18 @@ class light3DULABDataset(lightDPR7Dataset):
         else:
             real_img_path = os.path.join(self.opt.dataroot, 'real_im', "{:05d}".format(real_img_id) + '.png')
 
+        if real_img_id2 is not None:
+            if self.use_large_real_set2:
+                real_img_path2 = os.path.join(self.opt.load_realD2, "{:06d}".format(real_img_id2) + '.png')
+            else:
+                real_img_path2 = os.path.join(self.opt.load_realD2, "{:05d}".format(real_img_id2) + '.png')
+
         orig_img_path = os.path.join(target_path.rsplit('/',1)[0], 'orig.png')
         segment_img_path = os.path.join(self.opt.dataroot,'segments',source_path.split('/')[-2],AB_path[0].split('/')[-2]+'.png')
-
-        return real_img_path, orig_img_path, source_path, target_path, segment_img_path, src_light_path, tgt_light_path
+        if real_img_id2 is not None:
+            return real_img_path, orig_img_path, source_path, target_path, segment_img_path, src_light_path, tgt_light_path, real_img_path2
+        else:
+            return real_img_path, orig_img_path, source_path, target_path, segment_img_path, src_light_path, tgt_light_path
 
 
     def name(self):
